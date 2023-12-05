61b06869ad268d4322355883a5504e74c52d68c1
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-12-05 01:00:10 -0600

    created projects and contact me

--- portfolio-react/src/components/contact/Contact.jsx
+++ portfolio-react/src/components/contact/Contact.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './contact.css'
+import { MdEmail } from "react-icons/md";
+import { FaFacebookMessenger } from "react-icons/fa";
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rk7cl5l', 'template_ah00uyo', form.current, 'BmuE7nhCGAw5u0usz')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className="contact__option">
+            <MdEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>bcmonda22@gmail.com</h5>
+            <a href="mailto:bcmonda22@gmail.com" rel="noreferrer" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <FaFacebookMessenger className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Brian Monda</h5>
+            <a href="https://facebook.com/brian.monda.9" rel="noreferrer" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* End of Contact options */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- portfolio-react/src/components/portfolio/Portfolio.jsx
+++ portfolio-react/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/password.png'
+import IMG2 from '../../assets/Note-Taker.png'
+import IMG3 from '../../assets/logo-maker.png'
+import IMG4 from '../../assets/readme.png'
+import IMG5 from '../../assets/text-editor.webp'
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Password Generator',
+    github: 'https://github.com/bcm22/password_generator',
+    demo: 'https://bcm22.github.io/password_generator/',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Note Taker',
+    github: 'https://bcm22.github.io/note_taker/',
+    demo: 'https://immense-castle-52309-493bf1a5217d.herokuapp.com/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Logo Generator',
+    github: 'https://github.com/bcm22/logo_generator',
+    demo: 'https://github.com/bcm22/logo_generator',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'ReadMe Generator',
+    github: 'https://github.com/bcm22/readme_generator/blob/main/README.md',
+    demo: 'https://github.com/bcm22/readme_generator/blob/main/README.md',
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'PWA Text Editor',
+    github: 'https://github.com/bcm22/pwa-text-editor',
+    demo: 'https://bcm22-jate-5a6e859a0464.herokuapp.com/',
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
+                  <a href={demo} className='btn btn-primary' rel="noreferrer" target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
