bf42224be8175b0fa96f5077e9298f2126bfeb91
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-12-19 14:01:13 -0600

    socials

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import { FaGithub } from "react-icons/fa6";
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="www.linkedin.com/in/brian-monda-2b1493279/" target="_blank"><FaLinkedin /></a>
+        <a href="https://www.linkedin.com/in/brian-monda-2b1493279/" target="_blank"><FaLinkedin /></a>
         <a href="https://github.com/bcm22" target="_blank"><FaGithub /></a>
         </div>
   )
