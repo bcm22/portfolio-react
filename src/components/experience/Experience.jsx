99d84e360add71d8ed567804d0370fd0bdb89b72
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-12-28 18:00:11 -0600

    edited experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -7,7 +7,6 @@ const Experience = () => {
     <section id='experience'>
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
-
       <div className="container experience__container">
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
